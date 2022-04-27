f1ed564aac2e239f3ba354ec7e98c0a606b0b3c5
ek-test <ek51840@ubt-uni.net>
2022-04-27 21:27:41 +0200

    Food crud

---
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Models/Food.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantManagementSystem.Models
+{
+    public class Food
+    {
+        public int food_id { get; set; }
+        public string food_type { get; set; }
+        public string food_name { get; set; }   
+        public float food_price { get; set; }   
+      
+    }
+}
