a78082652c9379f2f9879267b9a935cd952a34df
ek-test <ek51840@ubt-uni.net>
2022-04-27 21:25:42 +0200

    Added new table (Food)

--- RestaurantManagementSystemDB.sql
+++ RestaurantManagementSystemDB.sql
@@ -29,3 +29,14 @@ insert into employee values
 ('Arlind Berisha', 'arlind', 'arlind123', '044271270', 'Peje', '2022-04-10', 1)
 
 select * from employee
+
+create table food(
+food_id int primary key identity,
+food_type varchar(150),
+food_name varchar(150),
+food_price float
+)
+
+insert into food values('Fastfood', 'Hamburger', '1.3')
+
+select * from food
