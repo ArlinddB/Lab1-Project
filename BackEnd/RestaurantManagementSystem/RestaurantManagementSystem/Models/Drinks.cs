2dfe778a215b02c9e547d42153c05e5d608a129e
Vanesa Hoxha <vh53857@ubt-uni.net>
2022-05-03 17:22:40 +0300

    Drinks class

---
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Models/Drinks.cs
@@ -0,0 +1,12 @@
+﻿namespace RestaurantManagementSystem.Models
+{
+    public class Drinks
+    {
+        public int drink_id { get; set; }
+        public string drink_type { get; set; }
+        public string drink_name { get; set; }
+
+        public float drink_price { get; set; }
+
+    }
+}
