da4f0b2e4f78dac2f57a8bafb35cbe33e2ef4e46
ArlinddB <93322395+ArlinddB@users.noreply.github.com>
2022-06-01 17:09:39 +0200

    Entity Framework

---
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿global using Microsoft.EntityFrameworkCore;
+using RestaurantManagementSystem.Models;
+
+namespace RestaurantManagementSystem.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Food> Foods { get; set; }
+
+        public DbSet<Drinks> Drinks { get; set; }
+
+    }
+}
