e4808baf44e7c316fd0ba56d416aa0aa8161fc10
ArlinddB <93322395+ArlinddB@users.noreply.github.com>
2022-06-01 17:10:24 +0200

    Entity Framework

--- BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Program.cs
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Program.cs
@@ -1,26 +1,42 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+global using RestaurantManagementSystem.Data;
+global using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Serialization;
 
-namespace RestaurantManagementSystem
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+builder.Services.AddCors();
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DataContext>(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseCors(options =>
+{
+    options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
