737701555c7be2fd61b70e10dea9990ac8626014
ArlinddB <93322395+ArlinddB@users.noreply.github.com>
2022-06-01 17:06:38 +0200

    Entity Framework

--- BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Models/Drinks.cs
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Models/Drinks.cs
@@ -1,11 +1,13 @@
-﻿namespace RestaurantManagementSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantManagementSystem.Models
 {
     public class Drinks
     {
+        [Key]
         public int drink_id { get; set; }
         public string drink_type { get; set; }
         public string drink_name { get; set; }
-
         public float drink_price { get; set; }
 
     }
