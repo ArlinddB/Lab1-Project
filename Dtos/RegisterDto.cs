56457c750e53423ee74f4e977279cba5c08c06fb
ArlinddB <93322395+ArlinddB@users.noreply.github.com>
2022-06-08 20:54:22 +0200

    Delete Dtos directory

--- Dtos/RegisterDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RestaurantManagementSystem.Dtos
-{
-    public class RegisterDto
-    {
-        public string Name { get; set; }    
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
