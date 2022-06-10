09ad91762121644ea78f08d9618f62ec4558c594
ArlinddB <93322395+ArlinddB@users.noreply.github.com>
2022-06-10 19:12:55 +0200

    Small change

---
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Migrations/20220610164800_RolesTable.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantManagementSystem.Migrations
+{
+    public partial class RolesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    roleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    roleName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.roleId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
---
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Migrations/20220610164958_EmployeesTable.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantManagementSystem.Migrations
+{
+    public partial class EmployeesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    e_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    e_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    e_username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    e_password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    e_phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    e_address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfJoining = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RoleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.e_id);
+                    table.ForeignKey(
+                        name: "FK_Employees_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "roleId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_RoleId",
+                table: "Employees",
+                column: "RoleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
