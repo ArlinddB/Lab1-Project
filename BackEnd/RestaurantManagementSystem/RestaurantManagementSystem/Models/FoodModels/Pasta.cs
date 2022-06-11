05fa5f97e5600ad976db736dd5606175c1eecaf6
Eroula <93322584+Eroula@users.noreply.github.com>
2022-06-11 12:16:12 +0200

    Food categories and food tables

---
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Models/FoodModels/Pasta.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace RestaurantManagementSystem.Models.FoodModels
+{
+    public class Pasta
+    {
+        [Key]
+        public int FoodId { get; set; }
+        public string FoodName { get; set; }
+        public float FoodPrice { get; set; }
+        public string FoodDescription { get; set; }
+
+        [ForeignKey("categoryId")]
+        public int categoryId { get; set; }
+        public FoodCategory category { get; set; }
+    }
+}
---
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Models/FoodModels/TraditionalFood.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestaurantManagementSystem.Models.FoodModels
+{
+    public class TraditionalFood
+    {
+        [Key]
+        public int FoodId { get; set; }
+        public string FoodName { get; set; }
+        public float FoodPrice { get; set; }
+        public string FoodDescription { get; set; }
+
+        [ForeignKey("categoryId")]
+        public int categoryId { get; set; }
+        public FoodCategory category { get; set; }
+    }
+}
