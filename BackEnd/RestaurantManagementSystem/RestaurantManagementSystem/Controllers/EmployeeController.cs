20e5600951e0afd3185f9d8cfb82b1476e461113
ArlinddB <93322395+ArlinddB@users.noreply.github.com>
2022-06-14 19:33:40 +0200

    Employee dto

--- BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Controllers/EmployeeController.cs
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Controllers/EmployeeController.cs
@@ -11,12 +11,13 @@ using RestaurantManagementSystem.Models;
 using Microsoft.AspNetCore.Hosting;
 using System.IO;
 using Microsoft.AspNetCore.Authorization;
+using RestaurantManagementSystem.Dtos;
 
 namespace RestaurantManagementSystem.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+    //[Authorize]
 
     public class EmployeeController : ControllerBase
     {
@@ -42,40 +43,53 @@ namespace RestaurantManagementSystem.Controllers
         [HttpGet("{empId}")]
         public async Task<ActionResult<List<Employee>>> GetEmpById(int empId)
         {
-            var emps = await _context.Employees.FirstOrDefaultAsync(c => c.e_id == empId);
+            var emps = await _context.Employees.Include(c => c.Role).FirstOrDefaultAsync(c => c.e_id == empId);
             return Ok(emps);
         }
 
         [HttpPost]
-        public async Task<ActionResult<List<Employee>>> AddEmployee(Employee emp)
+        public async Task<ActionResult<List<Employee>>> AddEmployee(InsertEmployeeDto empDto)
         {
-            if (emp == null)
+            if (empDto == null)
                 return BadRequest();
 
-            _context.Entry(emp.Role).State = EntityState.Unchanged;
-            _context.Employees.Add(emp);
+
+            var employee = new Employee 
+            { 
+                e_name = empDto.e_name,
+                e_username = empDto.e_username,
+                e_password = empDto.e_password,
+                e_phone = empDto.e_phone,
+                e_address = empDto.e_address,
+                DateOfJoining = empDto.DateOfJoining,
+                roleId = empDto.roleId,
+            };
+
+            _context.Employees.Add(employee);
             await _context.SaveChangesAsync();
-           
+
             return Ok("Added succesfuly");
         }
 
         [HttpPut("{empId}")]
-        public async Task<ActionResult<List<Employee>>> UpdateEmp(int empId, Employee emp)
+        public async Task<ActionResult<List<Employee>>> UpdateEmp(int empId, InsertEmployeeDto empDto)
         {
             var empUpdate = await _context.Employees.FirstOrDefaultAsync(e => e.e_id == empId);
             if (empUpdate == null)
                 return BadRequest("Employee not found.");
 
-            empUpdate.e_name = emp.e_name;
-            empUpdate.e_username = emp.e_username;
-            empUpdate.e_password = emp.e_password;
-            empUpdate.e_phone = emp.e_phone;
-            empUpdate.e_address = emp.e_address;
-            empUpdate.DateOfJoining = emp.DateOfJoining;
-            empUpdate.roleId = emp.roleId;
-            empUpdate.Role = emp.Role;
+            empUpdate.e_name = empDto.e_name;
+            empUpdate.e_username = empDto.e_username;
+            empUpdate.e_password = empDto.e_password;
+            empUpdate.e_phone = empDto.e_phone;
+            empUpdate.e_address = empDto.e_address;
+            empUpdate.DateOfJoining = empDto.DateOfJoining;
+            empUpdate.roleId = empDto.roleId;
+
+            _context.Employees.Update(empUpdate);
 
             await _context.SaveChangesAsync();
+
 
             return Ok("Updated successfuly");
         }
@@ -85,7 +99,7 @@ namespace RestaurantManagementSystem.Controllers
         {
             var empDelete = await _context.Employees.FindAsync(id);
             if (empDelete == null)
-                return BadRequest("Role not found.");
+                return BadRequest("Employee not found.");
 
             _context.Employees.Remove(empDelete);
             await _context.SaveChangesAsync();
