21c3d1f798c4db2efb10d70ce9468dd226c54c24
ArlinddB <93322395+ArlinddB@users.noreply.github.com>
2022-06-14 19:25:45 +0200

    .

--- BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Data/DataContext.cs
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Data/DataContext.cs
@@ -1,6 +1,7 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 using RestaurantManagementSystem.Models;
-//using RestaurantManagementSystem.Models.User;
+using RestaurantManagementSystem.Models.DrinksModels;
+using RestaurantManagementSystem.Models.FoodModels;
 
 namespace RestaurantManagementSystem.Data
 {
@@ -10,11 +11,20 @@ namespace RestaurantManagementSystem.Data
 
         public DbSet<Roles> Roles { get; set; }
         public DbSet<Employee> Employees { get; set; }
-
-        public DbSet<Food> Foods { get; set; }
-
-        public DbSet<Drinks> Drinks { get; set; }
-
         public DbSet<User> Users { get; set; }
+
+        public DbSet<FoodCategory> FoodCategories { get; set; } 
+
+        public DbSet<SeaFood> SeaFoods { get; set; }
+
+        public DbSet<FastFood> FastFoods { get; set; }
+
+        public DbSet<Pasta> Pastas { get; set; }
+
+        public DbSet<TraditionalFood> TraditionalFoods { get; set; }
+
+        
+
+       
     }
 }
