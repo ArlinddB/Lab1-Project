8d19445750d6cff7eeba1ea8f3947f31262e0163
ArlinddB <93322395+ArlinddB@users.noreply.github.com>
2022-06-14 19:31:05 +0200

    Employee dto

---
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Dtos/EmployeeDto/InsertEmployeeDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestaurantManagementSystem.Dtos
+{
+    public class InsertEmployeeDto
+    {
+        [Key]
+        public int e_id { get; set; }
+        public string e_name { get; set; }
+        public string e_username { get; set; }
+        public string e_password { get; set; }
+        public string e_phone { get; set; }
+        public string e_address { get; set; }
+        public string DateOfJoining { get; set; }
+
+        [ForeignKey("roleId")]
+        public int roleId { get; set; }
+    }
+}
