9336885230d7c4823fd439093c3012551c7fa15d
ArlinddB <93322395+ArlinddB@users.noreply.github.com>
2022-06-16 01:23:40 +0200

    Table, Reservation and Dashboard

---
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Models/DrinksModels/NonAlcoholicDrink.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestaurantManagementSystem.Models.DrinksModels
+{
+    public class NonAlcoholicDrink
+    {
+        [Key]
+        public int DrinkId { get; set; }
+        public string DrinkName { get; set; }
+        public float DrinkPrice { get; set; }
+        public string DrinkDescription { get; set; }
+
+        [ForeignKey("categoryId")]
+        public int categoryId { get; set; }
+        public DrinksCategory category { get; set; }
+    }
+}
