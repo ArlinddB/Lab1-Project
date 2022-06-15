6e1c427eda77541bb97b1c1d64bb877248ed5598
ArlinddB <93322395+ArlinddB@users.noreply.github.com>
2022-06-16 01:22:13 +0200

    Reservation Dto

---
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Dtos/ReservationDto/InsertReservationDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestaurantManagementSystem.Dtos.ReservationDto
+{
+    public class InsertReservationDto
+    {
+        [Key]
+        public int ReservationId { get; set; }
+        public string ClientName { get; set; }
+        public string DateAndTime { get; set; }
+        public string ClientContact { get; set; }
+
+        [ForeignKey("TableId")]
+        public int TableId { get; set; }
+    }
+}
