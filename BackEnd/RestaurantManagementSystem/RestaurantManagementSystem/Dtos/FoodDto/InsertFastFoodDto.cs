aec308ecd3d7146a9edc9574621c62523101d239
Eroula <93322584+Eroula@users.noreply.github.com>
2022-06-15 11:09:40 +0200

    Food Dto

---
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Dtos/FoodDto/InsertFastFoodDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestaurantManagementSystem.Dtos.FoodDto
+{
+    public class InsertFastFoodDto
+    {
+        [Key]
+        public int FoodId { get; set; }
+        public string FoodName { get; set; }
+        public float FoodPrice { get; set; }
+        public string FoodDescription { get; set; }
+
+        [ForeignKey("categoryId")]
+        public int categoryId { get; set; }
+    }
+}
