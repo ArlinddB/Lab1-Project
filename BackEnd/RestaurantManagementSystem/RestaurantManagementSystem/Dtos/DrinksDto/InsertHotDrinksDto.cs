2d221d37dca1bba2a89865f6b92fe22f64d324c4
Vanesahoxha <93322650+Vanesahoxha@users.noreply.github.com>
2022-06-15 11:10:58 +0200

    Drinks Dtos

---
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Dtos/DrinksDto/InsertHotDrinksDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestaurantManagementSystem.Dtos.DrinksDto
+{
+    public class InsertHotDrinksDto
+    {
+        [Key]
+        public int DrinkId { get; set; }
+        public string DrinkName { get; set; }
+        public float DrinkPrice { get; set; }
+        public string DrinkDescription { get; set; }
+
+        [ForeignKey("categoryId")]
+        public int categoryId { get; set; }
+    }
+}
