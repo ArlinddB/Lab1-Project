2c903053418595e6165c4724984c759f3cdea448
ArlinddB <93322395+ArlinddB@users.noreply.github.com>
2022-06-20 23:02:04 +0200

    Login update

--- FrontEnd/rms-f/src/components/Login/Login.js
+++ FrontEnd/rms-f/src/components/Login/Login.js
@@ -1,32 +1,88 @@
-import React, { Component } from "react";
-import {Button, Col, Form} from 'react-bootstrap';
+import React, { useState } from "react";
+import { Button, Col, Form } from "react-bootstrap";
+import { Redirect } from "react-router";
 
-export class Login extends Component{
-    render(){
-        return(
-            <>
-                <div className="container d-flex justify-content-center align-items-center loginContainer">
-                
-                    <Col centered>
-                        <Form>
-                            <Form.Group className="mb-3" controlId="formBasicEmail" size="sm">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" id="email" placeholder="Enter email" required/>
-                            </Form.Group>
+function Login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [redirect, setRedirect] = useState(false);
 
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" id="password" placeholder="Password" required/>
-                            </Form.Group>
-                         
-                            <Button type="submit" id="loginBtn" className="btn btn-primary btn-block loginBtn">
-                                Submit
-                            </Button>       
-                        </Form>
-                    </Col>
-                
-                </div>
-            </>
-        )
+
+  const loginUser = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch('https://localhost:5001/api/auth/login', {
+      method: 'POST',
+      headers: { 
+        'Accept':'application/json',
+        'Content-Type':'application/json'
+        },
+      credentials: 'include',
+      body: JSON.stringify(data)
+    });
+
+    const content = await response.json();
+    const txt = "Invalid credentials"
+
+    if(content === txt){
+      alert(content);
+      setRedirect(false);
     }
+    else{
+      setRedirect(true);
+    }
+  };
+
+
+  const data ={
+    email: email,
+    password: password
+  }
+
+  if(redirect){
+    return <Redirect push to="/dashboard" />
+  }
+
+  return (
+    <>
+      <div className="container d-flex justify-content-center align-items-center loginContainer">
+        <Col>
+          <Form onSubmit={loginUser}>
+            <Form.Group className="mb-3">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Enter email"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+
+            <Button
+              type="submit"
+              id="loginBtn"
+              className="btn btn-primary btn-block loginBtn"
+            >
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </div>
+    </>
+  );
 }
+export default Login;
