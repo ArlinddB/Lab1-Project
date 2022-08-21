8a2cd770e91d98488bf37293a84da35e659b50eb
Erola <93322584+ErolaKololli@users.noreply.github.com>
2022-08-21 20:08:54 +0200

    IdentityUser

--- BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Data/DataContext.cs
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Data/DataContext.cs
@@ -1,17 +1,20 @@
 ﻿global using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using RestaurantManagementSystem.Models;
 using RestaurantManagementSystem.Models.DrinksModels;
 using RestaurantManagementSystem.Models.FoodModels;
+using RestaurantManagementSystem.Models.ReservationModel;
+using RestaurantManagementSystem.Models.TableModel;
 
 namespace RestaurantManagementSystem.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<IdentityUser>
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         public DbSet<Roles> Roles { get; set; }
         public DbSet<Employee> Employees { get; set; }
-        public DbSet<User> Users { get; set; }
 
         public DbSet<FoodCategory> FoodCategories { get; set; } 
 
@@ -23,8 +26,21 @@ namespace RestaurantManagementSystem.Data
 
         public DbSet<TraditionalFood> TraditionalFoods { get; set; }
 
-        
+        public DbSet<DrinksCategory> DrinksCategories { get; set; }
 
+        public DbSet<NonAlcoholicDrink> NonAlcoholicDrinks { get; set; }
+
+        public DbSet<AlcoholicDrink> AlcoholicDrinks { get; set; }   
+
+        public DbSet<ColdDrink> ColdDrinks { get; set; }
+
+        public DbSet<HotDrink> HotDrinks { get; set; }
+
+        public DbSet<Table> Tables { get; set; } 
+
+        public DbSet<Reservation> Reservations { get; set; }
+
+        public DbSet<Salad> Salads { get; set; }
        
     }
 }
