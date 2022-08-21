cc631a3f3b4a36a8f40a728e5719b1f62c0fdc6c
Vanesahoxha <93322650+Vanesahoxha@users.noreply.github.com>
2022-08-21 20:31:40 +0200

    Drinks and Table Models

---
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Models/DrinksModels/DrinksCategory.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace RestaurantManagementSystem.Models.DrinksModels
+{
+    public class DrinksCategory
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+       
+    }
+}
---
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Models/TableModel/Table.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantManagementSystem.Models.TableModel
+{
+    public class Table
+    {
+        [Key]
+        public int TableId { get; set; }
+        public int NumberOfChairs { get; set; }
+        public string Place { get; set; }
+    }
+}
