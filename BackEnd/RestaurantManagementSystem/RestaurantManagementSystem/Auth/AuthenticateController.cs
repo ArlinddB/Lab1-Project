c0ee9f3e05e4012cad2c23cca9d7a8996fa60a88
Vanesahoxha <93322650+Vanesahoxha@users.noreply.github.com>
2022-08-21 20:47:24 +0200

    Login backend

--- BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Auth/AuthenticateController.cs
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Auth/AuthenticateController.cs
@@ -59,6 +59,37 @@ namespace RestaurantManagementSystem.Auth
             return Ok(new Response { Status = "Success", Message = "User created successfully!" });
         }
 
+	  [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+
+                foreach (var userRole in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                }
+
+                var token = GetToken(authClaims);
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
+        }
+
         private JwtSecurityToken GetToken(List<Claim> authClaims)
         {
             var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
