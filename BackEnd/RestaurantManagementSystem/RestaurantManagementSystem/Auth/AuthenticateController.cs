abce2d0d975441e46eca819e24885c24cd7991b6
Erola <93322584+ErolaKololli@users.noreply.github.com>
2022-08-21 20:48:32 +0200

    Normal User register

--- BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Auth/AuthenticateController.cs
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Auth/AuthenticateController.cs
@@ -60,6 +60,27 @@ namespace RestaurantManagementSystem.Auth
         }
 
 	  [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel model)
+        {
+            var userExists = await _userManager.FindByNameAsync(model.Username);
+            if (userExists != null)
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });
+
+            IdentityUser user = new()
+            {
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.Username
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." });
+
+            return Ok(new Response { Status = "Success", Message = "User created successfully!" });
+        }
+
+	  [HttpPost]
         [Route("login")]
         public async Task<IActionResult> Login([FromBody] LoginModel model)
         {
