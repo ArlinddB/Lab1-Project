6f5b65d6ef17ff5769b44ccb646be65eee05d98e
ArlinddB <93322395+ArlinddB@users.noreply.github.com>
2022-08-21 20:34:11 +0200

    Reservation Model

---
+++ BackEnd/RestaurantManagementSystem/RestaurantManagementSystem/Models/ReservationModel/Reservation.cs
@@ -0,0 +1,19 @@
+﻿using RestaurantManagementSystem.Models.TableModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestaurantManagementSystem.Models.ReservationModel
+{
+    public class Reservation
+    {
+        [Key]
+        public int ReservationId { get; set; }
+        public string ClientName { get; set; }
+        public string DateAndTime { get; set; }
+        public string ClientContact { get; set; }
+
+        [ForeignKey("TableId")]
+        public int TableId { get; set; }
+        public Table Table { get; set; }
+    }
+}
